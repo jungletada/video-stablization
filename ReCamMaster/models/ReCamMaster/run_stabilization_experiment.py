9ea15d2dc57497789c93ac3e0a0e80d5f6399f7b
jungletada <peng.dingjie@hotmail.com>
2026-06-04 15:07:50 +0900

    Update video frame transform for torchvision v2

--- ReCamMaster/models/ReCamMaster/run_stabilization_experiment.py
+++ ReCamMaster/models/ReCamMaster/run_stabilization_experiment.py
@@ -86,7 +86,8 @@ def make_fixed_camera_dataset_class():
                 [
                     v2.CenterCrop(size=(height, width)),
                     v2.Resize(size=(height, width), antialias=True),
-                    v2.ToDtype(torch.float32),
+                    v2.ToImage(),
+                    v2.ToDtype(torch.float32, scale=True),
                     v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
                 ]
             )
